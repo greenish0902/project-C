dd3c8174122ca3b05016078df16b3ec176352acd
greenish0902 <greenish0902@gmail.com>
2021-08-17 23:31:30 +0900

    C++ basic

---
+++ C++/hello_world.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int age = 23;
+  int loveNum = 9;
+  double PI = 3.141592;
+  printf("%d + %d = %d\n",age, loveNum, age+loveNum);
+  printf("hello world \n");
+  printf("I am %d years old\n", age);
+  printf("%.2f\n", PI);
+  printf("%g\n", PI);
+  printf("My name is %s\n", "sohui suh");
+  printf("Wanna study with %s?\n", "Shiloh");
+  return 0; 
+}
