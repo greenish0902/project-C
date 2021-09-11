324fd972775d01b1af0a65995492c0a7c320c7e3
greenish0902 <greenish0902@gmail.com>
2021-09-11 15:30:39 +0900

    C++ practices: bit operator

---
+++ C++/ex74_bit.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  char a = 12, b = 10;
+  printf("%d\n", a & b);
+  printf("%d\n", a | b );
+  printf("%d\n", a ^ b );
+  printf("%d\n", ~a );
+}
---
+++ C++/ex75_bit.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  char a = 22;
+  printf("%d\n", a << 1);
+  printf("%d\n", a << 3);
+  printf("%d\n", a << 6);
+  printf("%d\n", a >> 1);
+  printf("%d\n", a >> 3);
+  printf("%d\n", a >> 6);
+}
