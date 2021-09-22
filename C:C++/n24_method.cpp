563577153d5825501b05e00196aeea469d0078d2
greenish0902 <greenish0902@gmail.com>
2021-09-22 20:49:11 +0900

    C++ learn: method, operator overloading

---
+++ C:C++/n24_method.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+class Vector2 {
+public:
+  Vector2();
+  Vector2(float x, float y);
+
+  float GetX() const;
+  float GetY() const;
+  
+  // 정적 메서드
+  static Vector2 Sum(Vector2 a, Vector2 b) {
+    return Vector2(a.x + b.x, a.y + b.y);
+  }
+  
+  // 동적 메서드 
+  Vector2 Add(Vector2 rhs) {  // right-hand-side 우변
+    return Vector2(x + rhs.x, y + rhs.y);
+  }
+
+private:
+  float x;
+  float y;
+};
+
+int main() {
+  Vector2 a(2, 3);
+  Vector2 b(-1, 4);
+  Vector2 c1 = Vector2::Sum(a, b);
+  Vector2 c2 = a.Add(b);
+
+  cout << a.GetX() << ", " << a.GetY() << endl;
+  cout << b.GetX() << ", " << b.GetY() << endl;
+  cout << c1.GetX() << ", " << c1.GetY() << endl;
+  cout << c2.GetX() << ", " << c2.GetY() << endl;
+}
+
+Vector2::Vector2() : x(0), y(0) { }
+Vector2::Vector2(float x, float y) : x(x), y(y) { }
+float Vector2::GetX() const { return x; }
+float Vector2::GetY() const { return y; }
